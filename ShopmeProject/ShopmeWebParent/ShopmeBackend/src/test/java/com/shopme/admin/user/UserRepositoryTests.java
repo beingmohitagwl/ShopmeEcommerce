df106f2ded56a815657cc0b6844d28dc46a4eece
beingmohitagwl <beingmohitagwl@gmail.com>
2021-09-05 18:51:15 +0530

    Added code to change backend user enabled status. Closes #2

--- ShopmeProject/ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -100,4 +100,16 @@ public class UserRepositoryTests {
 		
 		assertThat(count).isNotNull().isGreaterThan(0);
 	}
+	
+	@Test
+	public void testDisableUser() {
+		Integer id = 1;
+		repo.updateEnabledStatus(id, false);
+	}
+	
+	@Test
+	public void testEnableUser() {
+		Integer id = 3;
+		repo.updateEnabledStatus(id, true);
+	}
 }
