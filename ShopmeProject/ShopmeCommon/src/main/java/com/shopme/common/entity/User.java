0b2c60215e124c6603a1c0fab4e5dc6d19e29f35
beingmohitagwl <beingmohitagwl@gmail.com>
2021-09-08 13:12:54 +0530

    Closes #3

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
@@ -1,5 +1,6 @@
 package com.shopme.common.entity;
 
+import java.beans.Transient;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -127,5 +128,11 @@ public class User {
 				+ ", roles=" + roles + "]";
 	}
 	
-	
+	@Transient
+	public String getPhotoImagePath() {
+		if(id == null || photos == null) {
+			return "/images/default-user.png";
+		}
+		return "/user-photos/" + this.id + "/" + this.photos;
+	}
 }
